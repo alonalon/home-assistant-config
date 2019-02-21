f68b2baf18c957de4abe6d014b4be7906ba369c0
alonalon <aronhafner@gmail.com>
2019-02-21 21:39:58 +0000

    Add global files for appdaemon and cleanup

---
+++ appdaemon/apps/common/globals.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+"""
+Global definitions that are used everywhere in my automations for easy access
+
+"""
+
+class GlobalEvents(Enum):
+    """
+        Add global events that are shared between componets
+    """

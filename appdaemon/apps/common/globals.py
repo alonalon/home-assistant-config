46c42bc0516b43eb290e98b673f06195c64fa814
Aron Hafner <aronhafner@gmail.com>
2019-02-26 22:42:09 +0100

    Add appdaemon presence app & config HA to use it (#17)
    * Add appdaemon presence app & config HA to use it
    
    * Fix name on presence app

--- appdaemon/apps/common/globals.py
+++ appdaemon/apps/common/globals.py
@@ -5,6 +5,22 @@ Global definitions that are used everywhere in my automations for easy access
 
 """
 
+presence_state = {}
+# Change this if you want to change the display name
+presence_state["home"] = "Home"
+presence_state["just_arrived"] = "Just arrived"
+presence_state["just_left"] = "Just left"
+presence_state["away"] = "Away"
+presence_state["extended_away"] = "Extended away"
+
+PEOPLE = {
+    'Aron': {
+        'device_tracker': 'person.aron',
+        'proximity': 'proximity.prox_home_aron',
+        'notifiers': ['ah']
+    }
+}
+
 class GlobalEvents(Enum):
     """
         Add global events that are shared between componets
